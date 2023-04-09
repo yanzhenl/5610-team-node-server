aaf20d40b79a62b02bca0c65556fc1aaa00f620b
evelynzz <zhao.yue3@northeastern.edu>
2023-04-08 19:43:11 -0700

    Enable login

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import express from 'express';
-import HelloController from "./controllers/hello-controller.js"
-const app = express()
-HelloController(app)
-app.listen(8000)
+import cors from "cors";
+import HelloController from "./controllers/hello-controller.js";
+import UsersController from "./users/users-controller.js";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+UsersController(app);
+HelloController(app);
+app.listen(8000);
