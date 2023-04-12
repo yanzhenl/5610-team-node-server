318b672df68e247c964a3d3e4656a41ae3e375ff
yanzhenl <li.yanzhe@northeastern.edu>
2023-04-12 16:08:16 -0700

    fixed mongo connection problem

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import express from 'express';
 import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-// mongoose.connect(CONNECTION_STRING);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING_TEAM
+mongoose.connect(CONNECTION_STRING);
 
-mongoose.connect('mongodb://127.0.0.1:27017/Farmer');
+// mongoose.connect('mongodb://127.0.0.1:27017/Farmer');
 
 const app = express();
 app.use(cors());
