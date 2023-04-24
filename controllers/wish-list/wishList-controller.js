875181687121bbe2534211112a69f96316b13921
yanzhenl <li.yanzhe@northeastern.edu>
2023-04-23 21:28:37 -0700

    enabled wishlist

--- controllers/wish-list/wishList-controller.js
+++ controllers/wish-list/wishList-controller.js
@@ -4,14 +4,8 @@ const WishListController = (app) => {
   const userAddWishList = async (req, res) => {
     const userId = req.params.uid;
     const productId = req.params.pid;
-    let wishList = await wishListDao.userAddWishList(userId, productId);
-    if (wishList) {
-      res.sendStatus(400);
-      return;
-    } else {
-      wishList = await wishListDao.userAddWishList(userId, productId);
-      res.json(wishList);
-    }
+    const wishList = await wishListDao.userAddWishList(userId, productId);
+    res.json(wishList);
   };
 
   const findWishListByUserId = async (req, res) => {
